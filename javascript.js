29a5c1b646449969ee1dec9c76276b6eb72a3d8e
NicholasWMurphy <nickmurphy68@icloud.com>
2025-03-07 17:55:03 +1100

    added ticker and bg3 review

--- javascript.js
+++ javascript.js
@@ -7,4 +7,23 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+function updateTime() {
+  const date = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const time = date.toLocaleTimeString();
+  const day = date.toLocaleDateString(undefined, options);
+  document.getElementById('time').innerHTML = `Today is: ${day}, Current Time: ${time}`;
+  document.getElementById('time2').innerHTML = `Today is: ${day}, Current Time: ${time}`;
+}
+
+setInterval(updateTime, 1000);
+window.onload = function() {
+  updateTime();
+};
+
+
+
+
+
+
 
